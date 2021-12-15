f8c0f7ceda9a4b47dc303e460991b4dd577df5a5
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-14 18:43:56 -0600

    Created personal indexing for tasks and added task deletion

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import "./App.css";
 
 class App extends Component {
   state = {
+    count: 1,
     taskList: [],
     task: {
+      taskCount: 0,
       text: '',
       id: uniqid()
     },
@@ -17,6 +19,7 @@ class App extends Component {
   addToList = (e) => {
     e.preventDefault();
     this.setState((prevState) => ({
+      count: prevState.count + 1,
       taskList: [...prevState.taskList, prevState.task],
       task : {
         text: '',
@@ -28,11 +31,19 @@ class App extends Component {
   updateValue = (e) => {
     this.setState({
       task: {
+        taskCount: this.state.count,
         text: e.target.value,
         id: this.state.task.id
       }
     });
   };
+
+  deleteTask = (index) => {
+    console.log(index);
+    this.setState(prevState => ({
+      taskList: prevState.taskList.filter(task => task !== prevState.taskList[index])
+    }))
+  }
 
   render() {
     return (
@@ -47,7 +58,7 @@ class App extends Component {
           />
           <input type="submit" value="submit" />
         </form>
-        <Overview arr={this.state.taskList} />
+        <Overview arr={this.state.taskList} deleteTask={this.deleteTask}/>
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-const Overview = ({arr}) => {
+const Overview = ({arr, deleteTask}) => {
   return(
     <div>
       {/* Task list should be generated here */}
       <ul>
-      {arr.map((task) => {
-        return <li key={task.id}>{task.text}</li>
+      {arr.map((task, index) => {
+        return <li className="task-item" key={task.id}>
+          {`${index + 1}. ${task.text}`}
+          <span onClick={() => deleteTask(index)}>Delete</span>
+          </li>
       })}
       </ul>
     </div>
